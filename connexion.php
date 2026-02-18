a1f4e0211401f25e374db46b4d942a643baf8799
imanehajji-tech <imane.hajji@3wa.io>
2026-02-18 09:37:19 +0000

    étape 1 : connexion

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "db.3wa.io";
+$port = "3306";
+$dbname = "imanehajji_phpj6";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "imanehajji";
+$password = "7cd2720849fe8bc55a5feb72a64cfc0c";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+var_dump($db);
+
+?>
