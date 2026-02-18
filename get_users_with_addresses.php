01bc0510ee566ad430041824c82ed1ab342cffb4
imanehajji-tech <imane.hajji@3wa.io>
2026-02-18 12:51:27 +0000

    étape 5 : get users with addresses

---
+++ get_users_with_addresses.php
@@ -0,0 +1,19 @@
+<?php
+
+require "connexion.php";
+
+$query = $db->prepare('
+    SELECT users.*, address.* 
+    FROM users
+    JOIN address ON users.address = address.id
+');
+
+$query->execute();
+
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($users);
+
+?>
+
+
